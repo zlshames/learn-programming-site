fd91c69ad2d65a7c20a1f95198a20c244e36b215
bkd705 <brennenkdenomme@gmail.com>
2017-01-30 16:52:36 -0500

    Started building new invite route

--- http/controllers/MainController.js
+++ http/controllers/MainController.js
@@ -7,10 +7,24 @@ class MainController {
     return res.sendFile(path.join(__dirname, '../../dist/index.html'))
   }
 
-  static createInvitee(req, res) {
-    console.log(req.knex)
-    return res.json("hey")
+  static getInvitees(req, res) {
+    req.knex.select('*').from('invitees')
+      .then(data => res.json(data))
+  }
+
+  static newInvite(req, res) {
+    const invitee = req.body
+    req.knex('invitees').insert({ 
+      name: invitee.name,
+      email: invitee.email,
+      position: invitee.position,
+      skill_level: invitee.skillLevel,
+      field: invitee.field
+    })
+    .then(response => {
+      console.log(response)
+    })
   }
 }
 
-module.exports = MainController
+export default MainController
--- http/routes.js
+++ http/routes.js
@@ -4,7 +4,9 @@ import MainController from './controllers/MainController'
 
 const routes = (app) => {
   // API routes
-  app.get('/api/v1/invitee', MainController.createInvitee)
+  app.get('/api/v1/invitee', MainController.getInvitees)
+
+  app.post('/api/v1/newinvite', MainController.newInvite)
 
   // Front-end route
   app.get('*', MainController.index)
